f43ba1a00b6d9e968cbe84b024d1e8c1fe833eaa
ultamatum <alexanderg0502@gmail.com>
2019-01-25 23:51:58 +0000

    Got character moving
    Character moving needs some polish
    Working on gravity system now

---
+++ Assets/Scripts/TouchCharacterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchCharacterController : MonoBehaviour
+{
+    Vector2 startPos;
+    Vector2 endPos;
+    Vector2 dir;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if(Input.touches.Length > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                startPos = touch.position;
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                endPos = touch.position;
+
+                dir = startPos - endPos;
+
+                this.GetComponent<Rigidbody2D>().AddForce(dir);
+
+                Debug.Log(dir);
+            }
+        }
+    }
+}
