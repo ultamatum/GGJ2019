22d1e1b70665088447593c89c84054cef6d3bbb9
ultamatum <alexanderg0502@gmail.com>
2019-01-26 02:26:32 +0000

    Physics complete, added trajectory dots
    Physics system with planetary orbits complete.
    
    Added a preview to the possible trajectory of the player but it'll get replaced cos it doesnt know what physics is

--- Assets/Scripts/TouchCharacterController.cs
+++ Assets/Scripts/TouchCharacterController.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class TouchCharacterController : MonoBehaviour
 {
     public float forceScalar = 1;
+    public GameObject trajectoryDotPrefab;
+    public const float DOT_TIME_STEP = 0.005f;
+    public const int NUM_DOTS_TO_SHOW = 20;
+
+    GameObject[] trajectoryDots = new GameObject[NUM_DOTS_TO_SHOW];
 
     Vector2 startPos;
-    Vector2 endPos;
-    Vector2 dir;
+    Vector2 velocity;
 
     void Start()
     {
-        
+        for(int i = 0; i < trajectoryDots.Length; i++)
+        {
+            trajectoryDots[i] = Instantiate(trajectoryDotPrefab);
+            trajectoryDots[i].transform.parent = this.gameObject.transform;
+            trajectoryDots[i].AddComponent<Rigidbody2D>();
+            trajectoryDots[i].SetActive(false);
+        }
     }
     
     void Update()
@@ -26,16 +36,40 @@ public class TouchCharacterController : MonoBehaviour
                 startPos = touch.position;
             }
 
-            if (touch.phase == TouchPhase.Ended)
+            if(touch.phase == TouchPhase.Moved)
             {
-                endPos = touch.position;
+                velocity = ((startPos - touch.position) * forceScalar);
 
-                dir = startPos - endPos;
+                DrawTrajectory();
+            }
 
-                this.GetComponent<Rigidbody2D>().AddForce(dir * forceScalar);
+            if (touch.phase == TouchPhase.Ended)
+            {                
+                this.GetComponent<Rigidbody2D>().AddForce(velocity);
 
-                Debug.Log(dir);
+                for (int i = 0; i < trajectoryDots.Length; i++)
+                {
+                    trajectoryDots[i].SetActive(false);
+                }
+
+                Debug.Log(velocity);
             }
         }
+    }
+
+    void DrawTrajectory()
+    {
+        for (int i = 1; i < NUM_DOTS_TO_SHOW; i++)
+        {
+            GameObject trajectoryDot = trajectoryDots[i];
+            trajectoryDots[i].SetActive(true);
+
+            trajectoryDot.transform.position= CalcPos(DOT_TIME_STEP * i);
+        }
+    }
+
+    private Vector2 CalcPos(float elapsedTime)
+    {
+        return (velocity * elapsedTime) + (Vector2)gameObject.transform.position;
     }
 }
