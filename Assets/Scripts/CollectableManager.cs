d1701d23b4e441cdadd449718b563b7bb296512f
ultamatum <alexanderg0502@gmail.com>
2019-01-26 18:16:26 +0000

    Collectable UI Complete
    Now shoes what jems have been collected

--- Assets/Scripts/CollectableManager.cs
+++ Assets/Scripts/CollectableManager.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class CollectableManager : MonoBehaviour
 {
     public GameObject[] collectables = new GameObject[3];
+    public GameObject[] scoreStorage = new GameObject[3];
 
     public void Reset()
     {
         for(int i = 0; i < collectables.Length; i++)
         {
             collectables[i].gameObject.SetActive(true);
+            scoreStorage[i].gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < collectables.Length; i++)
+        {
+            if(!collectables[i].activeSelf)
+            {
+                scoreStorage[i].gameObject.SetActive(true);
+            }
         }
     }
 }
