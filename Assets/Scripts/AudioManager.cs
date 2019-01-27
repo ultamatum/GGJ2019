4182194351d97491943cfcfc474e5a394a4bd5af
isobelkelly <44704947+isobelkelly@users.noreply.github.com>
2019-01-27 16:19:54 +0000

    Added new Menu screen and worked more on the audio code

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,9 +6,16 @@ public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
     }
 
     // Update is called once per frame
