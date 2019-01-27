f735a398c1e67639f672dc18a8ebc0ef28082709
ultamatum <alexanderg0502@gmail.com>
2019-01-27 03:51:12 +0000

    Main menu and level select created
    Framework for main menu and level select in place just need assets

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    public void LoadLevelByID(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void LoadLevelByName(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
