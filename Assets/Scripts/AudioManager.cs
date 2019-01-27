1c602169c83d8f5ac92998e266904cab8f3d5e75
isobelkelly <44704947+isobelkelly@users.noreply.github.com>
2019-01-27 16:44:21 +0000

    Audio continued

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,22 +5,46 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
-    // Start is called before the first frame update
+
+    public static AudioManager instance;
+
+    public AudioMixerGroup output;
+
     void Awake()
     {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
 
+            s.source.outputAudioMixerGroup = output;
+
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start ()
     {
-        
+        Play("Theme");
     }
+
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+
+    
 }
